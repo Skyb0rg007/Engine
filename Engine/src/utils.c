521e5ad55d098ddbf23948bab5f7dad2a6915f3d
Skye Soss <skyler.soss@gmail.com>
2018-10-29 23:57:15 -0400

    Ammendment

--- Engine/src/utils.c
+++ Engine/src/utils.c
@@ -1,7 +1,6 @@
 #include "common.h"
 #include "utils.h"
 
-
 /* load_file - convert a filepath into a content string
  * @path: the filepath
  *
@@ -14,20 +13,18 @@ ATTR_INTERNAL
 char *load_file(const char *path)
 {
     FILE *file = fopen(path, "rb");
-    if (file == NULL) {
-        FATAL("Could not open %s: %s", path, strerror(errno));
-    }
+    ASSERT(file != NULL);
 
     int ret = fseek(file, 0, SEEK_END);
-    ASSERT(ret != -1, "fseek: %s", strerror(errno));
+    ASSERT(ret != -1);
     int file_length = ftell(file);
-    ASSERT(file_length != -1, "ftell: %s", strerror(errno));
+    ASSERT(file_length != -1);
     rewind(file);
     char *data = calloc(file_length + 1, sizeof (char));
-    ASSERT(data != NULL, "Out of memory");
+    ASSERT(data != NULL);
     ret = fread(data, sizeof (char), file_length, file);
-    ASSERT(ret == file_length, "fread");
+    ASSERT(ret == file_length);
     ret = fclose(file);
-    ASSERT(ret != EOF, "fclose: %s", strerror(errno));
+    ASSERT(ret != EOF);
     return data;
 }
